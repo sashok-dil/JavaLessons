cfa216179253ce9ee58fd0b40d89dc011c93a53b
sashok-dil <alexander.kodola@gmail.com>
2014-12-04 15:08:49 +0200

    Update

--- ProjectATM/src/com/it/sources/atm/ATM.java
+++ ProjectATM/src/com/it/sources/atm/ATM.java
@@ -1,5 +1,8 @@
 package com.it.sources.atm;
 
 public class ATM {
+	//TODO Bag bag = new Bag()
+    //TODO ATMLogic logic = new ATMLogic()
+    //TODO insertClientValue(int clientValue)
 
 }
--- ProjectATM/src/com/it/sources/atm/money/Hryvna.java
+++ ProjectATM/src/com/it/sources/atm/money/Hryvna.java
@@ -4,6 +4,14 @@ public class Hryvna {
 	private int pat[] = {1,2,5,10,20,50,100,200,500};
 	private int value = 0;
 	
-	Hryvna
-
-}
+	Hryvnia(int value) {
+        this.value = value;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public int[] getPar() {
+        return par;
+    }
