f9f4ae7dba5460899211869220ec575053109a42
sashok-dil <alexander.kodola@gmail.com>
2014-12-05 10:09:09 +0200

    work

---
+++ ProjectATM/src/com/it/sources/atm/Box.java
@@ -0,0 +1,22 @@
+package com.it.sources.atm;
+
+import com.it.sources.atm.money.Money;
+
+public class Box {
+	
+	private Money[] bank;
+	private int i;
+	
+	public void add(Money money) {
+		
+	}
+	
+	public int sum() {
+		return 0;
+	}
+
+	public int size() {
+		return 0;
+	}
+
+}
--- ProjectATM/src/com/it/sources/atm/logic/ATMLogic.java
+++ ProjectATM/src/com/it/sources/atm/logic/ATMLogic.java
@@ -1,9 +1,23 @@
 package com.it.sources.atm.logic;
+import com.it.sources.atm.money.Money;
 
 public class ATMLogic {
 	//TODO create methods: consume, checkConterfeit, garbage
 	
-	public void consume(){
+	public ATMLogic() {
+		
+	}
+	public int CheckCounterfeit(int value, int[] values) {
+		return value;
+		
+	}
+	
+	public void consum(Money money) {
+		
+	}
+	
+	public void garbage() {
+		
 	}
 
 }
